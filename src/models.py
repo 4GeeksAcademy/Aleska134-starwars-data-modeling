6a02095b7153276ac42ee00207659cc426ac7b18
Aleska134 <143849441+Aleska134@users.noreply.github.com>
2023-12-27 18:23:46 +0000

    Aleska's first commit

--- src/models.py
+++ src/models.py
@@ -7,23 +7,65 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250))
+    lastname = Column(String(250))
+    email = Column(String(250))
+    password = Column(String(30), nullable=False)
+    subscription_date_mmddaaaa = Column (String(8))
 
-class Address(Base):
-    __tablename__ = 'address'
+    # def to_dict(self):
+    #     return {}
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    # street_number = Column(String(250))
+    # post_code = Column(String(250), nullable=False)
+    # person_id = Column(Integer, ForeignKey('person.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    # user_to_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    def to_dict(self):
+        return {}
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(250))
+    # street_number = Column(String(250))
+    # post_code = Column(String(250), nullable=False)
+    # person_id = Column(Integer, ForeignKey('person.id'))
+    favorite_id = Column(Integer, ForeignKey('favorite.id'))
+    # user_to_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(Favorite)
+
+    def to_dict(self):
+        return {}
+    
+class Character(Base):
+    __tablename__ = 'character'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String(250))
+    # street_number = Column(String(250))
+    # post_code = Column(String(250), nullable=False)
+    # person_id = Column(Integer, ForeignKey('person.id'))
+    favorite_id = Column(Integer, ForeignKey('favorite.id'))
+    # user_to_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(Favorite)
 
     def to_dict(self):
         return {}
